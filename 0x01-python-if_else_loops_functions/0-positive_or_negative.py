7a2ee2c5f13889334252b25b2eef63e9aeedd3bb
stepholo <oloobrian89@gmail.com>
2023-06-07 07:40:00 -0700

    Positive and Negative

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("{0} is {pve}". format(number, pve = "positive"))
+    print("{0} is {pve}". format(number, pve="positive"))
 elif number < 0:
-    print("{0} is {nve}". format(number, nve = "negative"))
+    print("{0} is {nve}". format(number, nve="negative"))
 elif number == 0:
-    print("{0} is {nll}". format(number, nll = "zero"))
+    print("{0} is {nll}". format(number, nll="zero"))
