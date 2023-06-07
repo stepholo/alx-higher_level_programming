f8c832f22823f4e077cdf937ce203af0a277ab6a
stepholo <oloobrian89@gmail.com>
2023-06-07 12:25:30 -0700

    Print Combination

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,4 @@ for i in range(100):
     if i < 10:
         print("{0}{1}".format(0, i), end=", ")
     else:
-        if i < 99:
-            print(i, end=", ")
-        else:
-            print(i)
+        print(i, end=", " if i < 99 else "\n")
