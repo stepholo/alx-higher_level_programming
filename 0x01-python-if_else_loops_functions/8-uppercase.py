b4741f560674ab3d852a848332920fc365d737c8
stepholo <oloobrian89@gmail.com>
2023-06-09 02:30:18 -0700

    Prints string into uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def uppercase(str):
+    ''' Converts the str to uppercase
+
+    Arg:
+       str - string to be converted to uppercase
+
+    Return:
+          The uppercase version of the str
+    '''
+    new_str = ""
+    for letter in str:
+        if 97 <= ord(letter) <= 122:
+            new_letter = chr(ord(letter) - 32)
+        else:
+            new_letter = letter
+
+        new_str += new_letter
+
+    print(new_str)
