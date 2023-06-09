ef3f12b98612ae16b4cd0a9cdc21aec0f50fa636
stepholo <oloobrian89@gmail.com>
2023-06-09 03:17:19 -0700

    Does additon

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    ''' Does additon
+
+    Arg:
+       a - first integer
+       b - second integer
+
+    Return:
+          Addition of a + b
+    '''
+    return a + b
