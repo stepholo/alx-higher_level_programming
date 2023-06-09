7957b64f824bfd11a4ce5ce2f0e27e32e72286bd
stepholo <oloobrian89@gmail.com>
2023-06-09 03:23:21 -0700

    Find a to power b

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def pow(a, b):
+    '''Computes the value of a to power b
+
+    Arg:
+       a - first integer
+       b - second integer
+
+    Return:
+          the value of a ^ b
+    '''
+    return a ** b
