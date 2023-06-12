f1b70494e40a62a5789d8119d5e46689080ec07e
stepholo <oloobrian89@gmail.com>
2023-06-12 05:01:35 -0700

    Print content of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,13 @@
+def print_list_integer(my_list=[]):
+    """
+    A function that prints content of a list
+
+    Parameter:
+     - my_list=[] - The list to get its content
+
+    Return:
+      - Prints content of the my_list assuming they are all integers
+
+    """
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]), end="\n")
