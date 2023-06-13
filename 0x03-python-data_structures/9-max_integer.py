8b88bdd87a5b5d270ca7afc544dac56862197341
stepholo <oloobrian89@gmail.com>
2023-06-13 03:04:23 -0700

    Find maximum integer of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """
+    A function that returns the maximum integer of a list
+    assuming the list contaings only integers
+
+    Parameters:
+       my_list: The containing the elements
+
+    Return:
+        The maximum integer  of my_list
+    """
+    ma_x = my_list[0]
+    for i in my_list:
+        if ma_x < i:
+            ma_x = i
+    return ma_x
