ac36c2db9891a9de79d9bc4bd9d40a27e546b141
stepholo <oloobrian89@gmail.com>
2023-06-13 02:34:35 -0700

    Tuple containing length and first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+def multiple_returns(sentence):
+    """
+    A function that returns a tuple with a length of
+    a string and its first character
+
+    Parameter:
+      Setence: The strings to get its length and first character
+
+    Return:
+      tuple containing length of setence and its first character
+    """
+    length = len(sentence)
+
+    if length == 0:
+        return (length, None)
+    else:
+        return (length, sentence[0])
