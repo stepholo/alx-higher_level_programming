7027488776c964512fb207b94efe320b4668bb6d
stepholo <oloobrian89@gmail.com>
2023-06-14 15:37:07 -0700

    Delete a key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pytho3
+def simple_delete(a_dictionary, key=""):
+    """ A function that deletes a key in a dictionary
+
+    Parameter:
+      - a_dictionary: The dictionary
+      - key: the key to delete
+
+    Return:
+      - The dictionary without the key
+    """
+    a = list(a_dictionary)
+    if key not in a:
+        return a_dictionary
+    elif key in a:
+        del a_dictionary[key]
+        return a_dictionary
