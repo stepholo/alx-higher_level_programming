960eb1e8422c531467f5f35bd5b92cb79a493b04
stepholo <oloobrian89@gmail.com>
2023-06-14 14:19:32 -0700

    The total number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """ Finds the number of keys in a dictionaty
+
+    Parameter:
+     - a_dictionay: The dictionaty to count the number of keys it contains
+
+    Return:
+      - The sum of the keys
+
+    """
+    return len(list(a_dictionary))
