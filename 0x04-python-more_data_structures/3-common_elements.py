4b5fbb65840b34a593ebc7358ead1f594f281abe
stepholo <oloobrian89@gmail.com>
2023-06-14 14:09:13 -0700

    Finds a common element in both sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """ Looks for a common set of element
+
+    Parameter:
+      - set_1: The first set
+      - set_2: The second set
+
+    Return: 
+       - a set of common elements in set_1 and set_2
+    """
+    return set_1 & set_2
