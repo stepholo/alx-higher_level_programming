227681cd071ba05a7bb99cda36535437e7263e3a
stepholo <oloobrian89@gmail.com>
2023-06-15 08:09:53 -0700

    Convert a roman number to arabic number

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+def roman_to_int(roman_sting):
+    """
+    A function that converts roman number to an arabic number
+
+    Parameter:
+     - roman_sting: The string containing the roman number
+
+    Return:
+     - An integer
+    """
+    from functools import reduce
+
+    dict_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    keys = list(dict_n)
+
+    roman = roman_sting
+    if type(roman) is not str:
+        return 0
+    elif roman is None:
+        return 0
+    else:
+        digit = []
+
+        for letter in roman:
+            for key in keys:
+                if key == letter:
+                    digit.append(dict_n[key])
+
+        i = 0
+        while i < len(digit) - 1:
+            if digit[i] < digit[i + 1]:
+                difference = digit[i + 1] - digit[i]
+                digit[i] = difference
+                del digit[i + 1]
+
+            else:
+                i += 1
+
+        arabic_number = reduce(lambda x, y: x + y, digit)
+
+        return arabic_number
