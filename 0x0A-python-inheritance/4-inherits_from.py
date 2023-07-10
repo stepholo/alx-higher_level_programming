0ba80e22f6a4747af49233e25272da639e25f11c
stepholo <oloobrian89@gmail.com>
2023-07-10 15:46:23 -0700

    using __mro__ magic method

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """using issubclass to check for inheritance
+    Arg:
+       obj: object to be checked
+       a_class: to to check in
+    Return:
+       True if obj if inherited from a_class else False
+    """
+    obj_classes = type(obj).__mro__[1:]
+
+    return any(a_class is cls for cls in obj_classes)
