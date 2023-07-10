ceaeafbfa1047212616f31808eb1d842791a9023
stepholo <oloobrian89@gmail.com>
2023-07-10 16:33:49 -0700

    defining class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+
+Defines an BaseGeometry
+
+
+"""
+
+
+class BaseGeometry:
+    """defines a class"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+
+Defines an BaseGeometry
+
+
+"""l
+
+
+class BaseGeometry:
+    """defines a class"""
+
+    def area(self):
+        """method to raise error message"""
+        raise Exception('area() is not implemented')
