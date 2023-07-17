2c1401a6e0494c5e63e002e7709a1c63eb3032a4
stepholo <oloobrian89@gmail.com>
2023-07-17 13:55:44 -0700

    child class square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,7 +2,6 @@
 """Module to define class Square"""
 
 
-import unittest
 from models.rectangle import Rectangle
 
 
@@ -19,3 +18,38 @@ class Square(Rectangle):
         y = self.y
         size = self.width
         return f"[Square] ({self.id}) {x}/{y} - {size}"
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """method to assign attributes to key and no key worded
+        argument
+        Parameter:
+            *arg: pointer to non key word argument
+            **kwargs: pointer to keyworded argument
+        """
+        if args:
+            attribute = ['id', 'size', 'x', 'y']
+            for i, arg in enumerate(args):
+                setattr(self, attribute[i], arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """method that returns dictionary representation of square"""
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+            }
