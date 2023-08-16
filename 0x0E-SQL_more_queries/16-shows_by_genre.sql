a1dbaceb2f215b77e1e0be367e3cd84cebbe501d
stepholo <oloobrian89@gmail.com>
2023-08-16 12:50:07 -0700

    lists all shows, and all genres linked to that show, from the database

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all shows and all genres linked to that show
+SELECT ts.title, IFNULL(tg.name, 'NULL') AS genre
+FROM tv_shows ts
+LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+LEFT JOIN tv_genres tg ON tsg.genre_id = tg.id
+ORDER BY ts.title ASC, tg.name ASC;
