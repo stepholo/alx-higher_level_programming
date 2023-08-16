9c5e31d4b406cd049382db54ccd02fca35800aa2
stepholo <oloobrian89@gmail.com>
2023-08-16 08:32:18 -0700

    Using Inner join

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Lists all cities contained in the database
+SELECT cities.id AS id, cities.name AS name, states.name AS name
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
