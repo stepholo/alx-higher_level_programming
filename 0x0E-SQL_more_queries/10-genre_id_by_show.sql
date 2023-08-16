06cfbf18bc759b5d802c5d64748b5998bca5d9a3
stepholo <oloobrian89@gmail.com>
2023-08-16 09:55:23 -0700

    use Inner loop

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 -- that have at least one genre linked
 SELECT s.`title`, g.`genre_id`
 FROM `tv_shows` AS s
-INNER JOIN `tv_shows_genres` AS g
+INNER JOIN `tv_show_genres` AS g
 ON s.`id` = g.`show_id`
 ORDER BY s.`title`, g.`genre_id` ASC;
