fa0c71c24895b864e1765aeae0aab6b39628c032
stepholo <oloobrian89@gmail.com>
2023-08-16 06:30:52 -0700

    Create table force_name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Creates a table force_name
+-- If the table force_name already exists, the sript should not faill
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT,
+	name VARCHAR(256) NOT NULL
+	);
