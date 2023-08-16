4bc83ac6f382b29c4444f451d750a602432b5d00
stepholo <oloobrian89@gmail.com>
2023-08-16 12:39:09 -0700

    list comedy shows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- List all genres where title = 'Dexter'
+SELECT ts.title
+FROM tv_shows ts
+JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+JOIN tv_genres tg ON tsg.genre_id = tg.id
+WHERE tg.name = 'Comedy'
+ORDER BY ts.title ASC;
