eea298894d0d16346c8610dba9edf05ef8e6d6d2
stepholo <oloobrian89@gmail.com>
2023-08-16 10:11:37 -0700

    left join

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Lists all shows contained in hbtn_0d_tvshow
+SELECT s.`title`, g.`genre_id`
+FROM `tv_shows` AS s
+LEFT JOIN `tv_show_genres` AS g
+ON s.`id` = g.`show_id`
+ORDER BY s.`title`, g.`genre_id` ASC;
