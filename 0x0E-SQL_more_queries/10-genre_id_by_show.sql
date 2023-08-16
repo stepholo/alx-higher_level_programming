c4e4929d85207d8289c8deffcd5f2c7ff94c0528
stepholo <oloobrian89@gmail.com>
2023-08-16 09:47:58 -0700

    use inner join

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Lists all shows contained in hbtn_0d_tvshows
+-- that have at least one genre linked
+SELECT s.`title`, g.`genre_id`
+FROM `tv_shows` AS s
+INNER JOIN `tv_shows_genres` AS g
+ON s.`id` = g.`show_id`
+ORDER BY s.`title`, g.`genre_id` ASC;
