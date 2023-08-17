d2a33ed54b7be4c6da06422c10d0b217a45b2628
stepholo <oloobrian89@gmail.com>
2023-08-17 10:37:22 -0700

    Limit by 3

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- Displays the average temperature(Fahr) by city
+-- ordered by temperature (descending)
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
