216a640cb1284e93f134447bb9f5e742dae3cb79
Stephen B. Oloo <oloobrian89@gmail.com>
2023-09-11 09:57:34 -0700

    Argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length === 2) {
+if (argv.length < 3) {
   console.log('No argument');
 } if (argv.length === 3) {
   console.log('Argument found');
