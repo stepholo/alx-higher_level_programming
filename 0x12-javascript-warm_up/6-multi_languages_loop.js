561c8a39a6e52985a1662c2d7fea2d981d4e6fe7
Stephen B. Oloo <oloobrian89@gmail.com>
2023-09-11 12:30:24 -0700

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+for (let i = 0; i < 3; i++) {
+  switch (i) {
+    case 0:
+      console.log('C is fun');
+      break;
+    case 1:
+      console.log('Python is cool');
+      break;
+    case 2:
+      console.log('JavaScript is amazing');
+      break;
+    default:
+      console.log('Unknown language');
+  }
+}
