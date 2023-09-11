22a21bab5ddfbebac5655f03fe4da3d0e34aff52
Stephen B. Oloo <oloobrian89@gmail.com>
2023-09-11 12:37:18 -0700

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,16 +1,5 @@
 #!/usr/bin/node
+const Arry = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for (let i = 0; i < 3; i++) {
-  switch (i) {
-    case 0:
-      console.log('C is fun');
-      break;
-    case 1:
-      console.log('Python is cool');
-      break;
-    case 2:
-      console.log('JavaScript is amazing');
-      break;
-    default:
-      console.log('Unknown language');
-  }
+	console.log(Arry[i])
 }
