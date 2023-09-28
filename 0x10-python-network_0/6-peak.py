7f11d282e4f0e7909ae46af76f46a840c6a1d487
stepholo <oloobrian89@gmail.com>
2023-09-28 14:00:20 -0700

    find the peak value

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module to define find_peak function
+"""
+
+
+def find_peak(list_of_integers):
+    """Function that finds a peak in a list of unsorted integers
+
+        Param:
+            list_of_integers: list of unsorted integers
+    """
+    if len(list_of_integers) == 0:
+        return None
+
+    elif len(list_of_integers) <= 2:
+        return max(list_of_integers)
+
+    new_list = []
+
+    for i in range(len(list_of_integers) - 1):
+        k = list_of_integers[i]
+        l = list_of_integers[i - 1]
+        m = list_of_integers[i + 1]
+        if k >= l and k >= m:
+            new_list.append(k)
+
+    if not new_list:
+        return max(list_of_integers)
+    
+    return find_peak(new_list)
