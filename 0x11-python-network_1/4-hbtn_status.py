9bc08d4223da4143cab67f95ffb940a527d55d77
stepholo <oloobrian89@gmail.com>
2023-09-29 06:23:18 -0700

    What's my staus

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response")
     print(f"\t- type: {type(r.text)}")
-    print(f"\t- content {r.content}")
+    print(f"\t- content {r.text}")
