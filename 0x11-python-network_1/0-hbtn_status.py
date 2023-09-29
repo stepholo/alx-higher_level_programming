b07d5cf492390e891e5a0c41cc2259565128590d
stepholo <oloobrian89@gmail.com>
2023-09-29 04:53:46 -0700

    What's my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that defines a function for fetching url
+"""
+
+
+import urllib.request
+
+
+def fetch_url(url):
+    """Function that fetches a given url
+    Param:
+       url - the provided url
+    Return:
+       type, content and utf8
+    """
+    with urllib.request.urlopen(url) as response:
+        contents = response.read()
+        utf8_content = contents.decode('utf-8')
+        print("Body response:")
+        print(f"    - type: {type(contents)}")
+        print(f"    - content: {contents}")
+        print(f"    - utf8 content: {utf8_content}")
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    fetch_url(url)
