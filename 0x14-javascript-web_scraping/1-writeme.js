9dbbd0819a40da5829e51ffa58965681905a1464
root <oloobrian89@gmail.com>
2023-10-24 02:22:02 -0700

    Write to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const fileName = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(fileName, content, 'utf8', err => {
+  if (err) {
+    console.log(err);
+  }
+});
