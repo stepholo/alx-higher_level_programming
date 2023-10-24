5c07807d0daf9c123335765dd487ff7701fc9000
root <oloobrian89@gmail.com>
2023-10-24 04:34:36 -0700

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+
+const request = require('request')
+
+const requestUrl = process.argv[2];
+const fileName = process.argv[3];
+
+request(requestUrl, (error, response, body) => {
+	if (error) {
+		console.error(error);
+		return;
+	}
+
+	if (response.statusCode === 200) {
+		fs.writeFile(fileName, body, 'utf8', err => {
+			if (err) {
+				console.error(err);
+			}
+		});
+	}
+});
