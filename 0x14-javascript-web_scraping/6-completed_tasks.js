5e1907740084c33e914b10d6757d29cca6a9f9c6
root <oloobrian89@gmail.com>
2023-10-24 05:49:59 -0700

    How many completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+
+  const tasks = JSON.parse(body);
+
+  const completedTasksByUser = {};
+
+  // Iterate through tasks and count completed tasks for each user
+  tasks.forEach(task => {
+    if (task.completed) {
+      if (completedTasksByUser[task.userId]) {
+        completedTasksByUser[task.userId]++;
+      } else {
+        completedTasksByUser[task.userId] = 1;
+      }
+    }
+  });
+  console.log(completedTasksByUser);
+});
