4eb4def1af53f77008d7a280e27c8454ab867c24
root <oloobrian89@gmail.com>
2023-10-24 04:15:30 -0700

    count star wars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
-const request = require('request')
+const request = require('request');
 
 const requestURL = process.argv[2];
-const characterurl = `https://swapi-api.alx-tools.com/api/people/18/`;
+const characterurl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request.get(requestURL, (error, response, body) => {
-	if (error) {
-		console.error(err);
-		return;
-	}
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-	const data = JSON.parse(body).results;
+  const data = JSON.parse(body).results;
 
-	const moviesWithWedge = data.filter(datas =>
-		datas.characters.includes(characterurl));
-	
-	console.log(`${moviesWithWedge.length}`);
+  const moviesWithWedge = data.filter(datas =>
+    datas.characters.includes(characterurl));
+
+  console.log(`${moviesWithWedge.length}`);
 });
