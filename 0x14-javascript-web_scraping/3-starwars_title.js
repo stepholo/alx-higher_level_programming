54c7f88eb980a29553c253a9f67fb0ef42ad39c1
root <oloobrian89@gmail.com>
2023-10-25 00:39:36 -0700

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
-const request = require('request')
+const request = require('request');
 
 const movieId = process.argv[2];
 const requestURL = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 request.get(requestURL, (error, response, body) => {
-	if (error) {
-		console.error(err);
-		return;
-	}
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-	const data = JSON.parse(body);
-	if (data.title) {
-		console.log(data.title);
-	}
+  const data = JSON.parse(body);
+  if (data.title) {
+    console.log(data.title);
+  }
 });
