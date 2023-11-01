6b40bfd4c26676e2ce5b579e14272924ba0f4fd7
stepholo <oloobrian89@gmail.com>
2023-11-01 12:03:59 -0700

    Added

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,5 @@
 // Get from an api
 
 $.get('https://swapi.co/api/people/5/?format=json', function (body) {
-   $('DIV#character').text(body['name']);
- });
+  $('DIV#character').text(body.name);
+});
